109b3643c3b8d652277e83e55abf38b3fed8a5c7
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-04-30 19:02:17 +0600

    Mongodb Compass connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 5001;
+require('dotenv').config();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,24 @@
 const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const { notFound, errorHandler } = require('./middleware/error.middleware');
-
+const connectDB = require('./config/db.config');
+// const cors = require('cors');
+// const helmet = require('helmet');
+// const { notFound, errorHandler } = require('./middleware/error.middleware');
+require('dotenv').config();
 const app = express();
+connectDB();
+// // Middleware
+// app.use(helmet());
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// Middleware
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// // Health check route
+// app.get('/api/health', (req, res) => {
+//   res.status(200).json({ status: 'OK' });
+// });
 
-// Health check route
-app.get('/api/health', (req, res) => {
-  res.status(200).json({ status: 'OK' });
-});
-
-// Error handling middleware
-app.use(notFound);
-app.use(errorHandler);
+// // Error handling middleware
+// app.use(notFound);
+// app.use(errorHandler);
 
 module.exports = app;
