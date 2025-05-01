236b80a646cac6fa9f067ecf2851a82ef420fedd
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-05-01 12:50:34 +0600

    connect to client

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,13 @@ connectDB();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
